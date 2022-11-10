cb1e4551b33a6b69e804f5e56b8e49698e6379cd
Cortan122 <Cortan122@gmail.com>
2022-11-10 18:30:14 +0300

    feat: orm models

--- src/database.py
+++ src/database.py
@@ -1,9 +1,16 @@
-from sqlalchemy import create_engine
+from sqlalchemy import Column, DateTime, Integer, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.sql import func
 
-from environment import DATABASE_URI
+from .environment import DATABASE_URI
 
 Base = declarative_base()
 engine = create_engine(DATABASE_URI, echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
+
+
+class TimestampMixin:
+    id = Column(Integer, primary_key=True, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
---
+++ src/models/User.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+
+from ..database import Base, TimestampMixin
+
+
+class User(TimestampMixin, Base):
+    __tablename__ = "telegram_users"
+    message_unix_time = Column(Integer, nullable=False)
+    state_name = Column(String(255), nullable=False)
