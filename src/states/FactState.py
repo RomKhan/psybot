e28277bc45fc7db6935c230b3af812e49d1597b3
alkmnd <blvantla@gmail.com>
2023-07-19 14:52:25 -0700

    fixed

--- src/states/FactState.py
+++ src/states/FactState.py
@@ -1,8 +1,12 @@
+from .RecommendationManager import RecommendationManager
 from ..util import flatten, load_data_file
 from .PageableState import PageableState
 
 
 class FactState(PageableState):
+    def print_recommendation(self) -> str:
+        return ""
+
     name = "Facts"
     random_button = "Случайный факт"
     start_button = "Факты"
@@ -14,4 +18,6 @@ class FactState(PageableState):
         return fact.split("\n")[0].split(". ")[0]
 
     def print_item(self) -> str:
+        self.recommendation_message = self.print_recommendation()
+        self.need_recommendation = True
         return f"Факт №{self.item_number+1}\n\n{self.items[self.item_number]}"
